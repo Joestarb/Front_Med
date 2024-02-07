4e55e871ce74c9e831a989605da4ab6e2fbfe4ae
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2024-02-07 13:09:51 -0500

    login terminado

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,23 +1,25 @@
 // screens/HomeScreen.js
-import React, { useEffect, useRef } from 'react';
-import { View } from 'react-native';
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import loginImg from "../../assets/LoginAssets/login.png";
 
 const HomeScreen = ({ navigation }) => {
-    const titleRef = useRef(null);
 
-    useEffect(() => {
-        titleRef.current.fadeIn(4000); // Puedes ajustar la animación según tus necesidades
-    }, []);
+
 
     return (
-        <View className="  bg-white h-screen  "  >
-            <Animatable.View ref={titleRef}>
+        <View className="  bg-white h-screen  flex items-center"  >
+            <View>
                 <Animatable.Image source={loginImg} className=" w-[350] h-[350]" />
-                <Animatable.Text className=" font-bold text-4xl" animation="slideInDown" iterationCount={1} direction="alternate">Inicia sesión</Animatable.Text>
-                <Animatable.Text className=" text-center" animation="slideInUp" iterationCount={1} direction="alternate" onPress={() => navigation.navigate('Index')}>Ir a TaskApp</Animatable.Text>
-            </Animatable.View>
+                <Text className=" font-bold text-4xl ml-8" >Inicia sesión</Text>
+                <View className="flex mb-3 gap-2">
+                <TextInput placeholder="Correo Electrónico" className=" border-2 p-4  rounded-xl" />
+                <TextInput placeholder="Contraseña" className=" border-2 p-4  rounded-xl" />
+                </View>
+                <Text className=" text-center bg-[#1178BD] text-white text-2xl  rounded-2xl  font-semibold p-2" onPress={() => navigation.navigate('Index')}>Iniciar Sesion</Text>
+                <Text className=" mt-3 text-center font-bold">No tienes cuenta? Create una</Text>
+            </View>
         </View>
     );
 };
