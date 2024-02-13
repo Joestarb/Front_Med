318000c7c0ebee48238c7faccc462a4eb5063725
Alexis Dolores <alexisdanieldr@gmail.com>
2024-02-13 16:28:51 -0500

    Screens add correct

--- screens/EditarTituloDoctor/EditarTitulo.js
+++ screens/EditarTituloDoctor/EditarTitulo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { Text, View } from 'react-native-animatable'
+import { Modal, Text, View } from 'react-native-animatable'
 import { Button, TextInput } from 'react-native-paper'
 
 export default function EditarTitulo() {
@@ -47,41 +47,6 @@ export default function EditarTitulo() {
                     style={{ underlineColorAndroid: 'transparent' }}
                     placeholder='Nombre de su especialidad'
                 />
-            </View>
-            <View>
-                {Platform.OS === 'ios' ? (
-                    <DatePickerIOS
-                        date={date}
-                        onDateChange={handleDateChange}
-                        mode="date"
-                    />
-                ) : (
-                    <Button title="Seleccionar fecha" onPress={showDatePickerAndroid} />
-                )}
-            </View>
-            <View>
-                <Button title="Abrir calendario" onPress={handleOpenModal} />
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={modalVisible}
-                    onRequestClose={handleCloseModal}
-                >
-                    <View className='flex-1 justify-center' style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-                        <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
-                            <DateTimePicker
-                                value={selectedDate}
-                                mode="date"
-                                is24Hour={true}
-                                display="default"
-                                onChange={handleDateChange}
-                                minimumDate={new Date(2020, 0, 1)}
-                                maximumDate={new Date(2025, 11, 31)}
-                            />
-                            <Button title="Cerrar" onPress={handleCloseModal} />
-                        </View>
-                    </View>
-                </Modal>
             </View>
 
         </View>
